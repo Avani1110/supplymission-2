ec9a2ec8e91a9c7f140c6fc8925845c9e184851c
Avani1110 <71072333+Avani1110@users.noreply.github.com>
2020-10-07 17:55:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,6 +59,7 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+	packageSprite.bounceOff(boxSide1)
   drawSprites();
  
 }
